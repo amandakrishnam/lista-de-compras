3f3c22f982f5ff86afc4ff88f4df3e0b6f81d36e
Amanda <47640818+amandakrishnam@users.noreply.github.com>
2025-08-11 15:27:16 -0300

    Adição de comentários verificarListaVazia.js

--- scripts/verificarListaVazia.js
+++ scripts/verificarListaVazia.js
@@ -1,12 +1,21 @@
+// Seleciona o elemento HTML com a classe "mensagem-lista-vazia"
+//  e armazena na variável
 const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+// Define a função que verifica se a lista de compras está vazia
 function verificarListaVazia(listaDeCompras) {
-    //pega todos os li, retorna um array
+    // Seleciona todos os elementos <li> dentro da lista de compras e 
+    // retorna um NodeList (array)
     const itensDaLista = listaDeCompras.querySelectorAll("li");
+    // Se não houver nenhum <li> na lista (lista vazia)
     if (itensDaLista.length === 0) {
+        // Exibe a mensagem de lista vazia
         mensagemListaVazia.style.display = "block";
     } else {
+        // Esconde a mensagem de lista vazia
         mensagemListaVazia.style.display = "none";
     }
 }
 
+// Exporta a função para ser usada em outros módulos
 export default verificarListaVazia;
